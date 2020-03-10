1ad10c51d8868e339dda0e0a397ce8fb780d3d77
gomezjc <gomezjc0523@gmail.com>
2020-03-10 15:29:56 -0500

    se agrega comportamiento de abrir y cerrar puerta con timeline en c++

--- Source/CppBasicUE4/Private/JC_Door.cpp
+++ Source/CppBasicUE4/Private/JC_Door.cpp
@@ -3,6 +3,7 @@
 
 #include "Public/JC_Door.h"
 #include "Components/StaticMeshComponent.h"
+#include "TimerManager.h"
 
 // Sets default values
 AJC_Door::AJC_Door()
@@ -18,27 +19,59 @@ AJC_Door::AJC_Door()
 	doorComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("door"));
 	doorComponent->SetupAttachment(customRootComponent);
 
+	MyTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("Timeline"));
+
+	InterpFunction.BindUFunction(this, FName("TimelineFloatReturn"));
+	TimelineFinished.BindUFunction(this, FName("OnTimelineFinished"));
 	OpenAngle = -90.0f;
+	
+	
 }
 
 // Called when the game starts or when spawned
 void AJC_Door::BeginPlay()
 {
 	Super::BeginPlay();
-	openDoor();
-	
-}
 
-void AJC_Door::openDoor()
-{
-	FRotator newRotation = FRotator(0.0f, OpenAngle, 0.0f);
-	doorComponent->SetRelativeRotation(newRotation);
+	if(fCurve)
+	{
+		MyTimeline->AddInterpFloat(fCurve, InterpFunction, FName("Alpha"));
+		MyTimeline->SetTimelineFinishedFunc(TimelineFinished);
+	}
+
+	StartAngle = doorComponent->GetRelativeTransform().Rotator();
+	EndAngle = FRotator(StartAngle.Pitch, OpenAngle, StartAngle.Roll);
+
+	MyTimeline->SetLooping(false);
+	MyTimeline->SetIgnoreTimeDilation(true);
+	MyTimeline->Play();
 }
 
 // Called every frame
 void AJC_Door::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+void AJC_Door::TimelineFloatReturn(float value)
+{
+	doorComponent->SetRelativeRotation(FMath::Lerp(StartAngle,EndAngle,value));
+}
+
+void AJC_Door::OnTimelineFinished()
+{
+	if(MyTimeline->GetPlaybackPosition() == 0.0f)
+	{
+		GLog->Log("Stop");
+		MyTimeline->Stop();
+	}else if(MyTimeline->GetPlaybackPosition() == this->MyTimeline->GetTimelineLength())
+	{
+		GLog->Log("reverse");
+		GetWorldTimerManager().SetTimer(MyTimerHandle, this, &AJC_Door::ReverseTimeLine, 0.5f, false);
+	}
+}
+
+void AJC_Door::ReverseTimeLine()
+{
+	MyTimeline->Reverse();
+}
--- Source/CppBasicUE4/Public/JC_Door.h
+++ Source/CppBasicUE4/Public/JC_Door.h
@@ -2,17 +2,21 @@
 
 #pragma once
 
+#include "Components/TimelineComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "JC_Door.generated.h"
 
 class USceneComponent;
 class UStaticMeshComponent;
+class UTimelineComponent;
 
 UCLASS()
 class CPPBASICUE4_API AJC_Door : public AActor
 {
 	GENERATED_BODY()
+
+		class UTimelineComponent* MyTimeline;
 	
 public:	
 	// Sets default values for this actor's properties
@@ -22,12 +26,34 @@ public:
 
 	UPROPERTY(VisibleAnywhere,Category="Door")
 	UStaticMeshComponent* doorFrameComponent;
-	
-	UPROPERTY(VisibleAnywhere, Category = "Door")
-	UStaticMeshComponent* doorComponent;
 
-	UPROPERTY(EditAnywhere,Category="Door")
-	float OpenAngle;
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+		class UCurveFloat* fCurve;
+
+	UPROPERTY(VisibleAnywhere, Category = "Door")
+		UStaticMeshComponent* doorComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Door")
+		float OpenAngle;
+	
+	UPROPERTY()
+		FRotator StartAngle;
+
+	UPROPERTY()
+		FRotator EndAngle;
+	
+	FOnTimelineFloat InterpFunction{};
+	FOnTimelineEvent TimelineFinished{};
+	FTimerHandle MyTimerHandle;
+
+	UFUNCTION()
+		void TimelineFloatReturn(float value);
+
+	UFUNCTION()
+		void OnTimelineFinished();
+
+	UFUNCTION()
+		void ReverseTimeLine();
 	
 protected:
 	// Called when the game starts or when spawned
@@ -36,7 +62,4 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	void openDoor();
-
 };
